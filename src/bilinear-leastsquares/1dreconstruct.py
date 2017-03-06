5829012000469a0005a81ce5cf6d4ef346e88a1d
James Shaw <js102@zepler.net>
2017-03-06 15:48:27 +0000

    start 1D least squares C--P reconstruction in python

---
+++ src/bilinear-leastsquares/1dreconstruct.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import numpy as np
+import numpy.linalg as la
+import math
+import os
+
+def init_schaer_radial(b, position, centre=(-50e3, 9e3), half_width=(25e3,3e3)):
+    Ax = half_width[0]
+    Az = half_width[1]
+    x0 = centre[0]
+    z0 = centre[1]
+    for i in range(b.shape[0]):
+        for k in range(b.shape[1]):
+            x, z = position((i,k))
+            r = math.sqrt(((x-x0)/Ax)**2 + ((z-z0)/Az)**2)
+            if r <= 1:
+                b[i,k] = math.cos(math.pi*r/2)**2
+
+def dump(b, position, f):
+    for i in range(b.shape[0]):
+        for k in range(b.shape[1]):
+            x, z = position((i,k))
+            if abs(b[i,k]) > 1e-14:
+                print(x, z, b[i,k], file=f)
+
+def b_position(index):
+    x = 0.5*delta[0] + delta[0]*index[0] - 0.5*n[0]*delta[0]
+    z = delta[1]*index[1]
+    return (x, z)
+
+def c_position(index):
+    x = 0.5*dx + dx*index[0] - 0.5*n[0]*dx
+    z = dz*index[1]
+    return (x, z+0.5*dz)
+
+def reconstruct(b):
+    c = np.zeros((b.shape[0], b.shape[1]-1))
+    # create a matrix and invert it
+    # we can reuse the same coefficients for every cell
+    for i in range(c.shape[0]):
+        for k in range(b.shape[1]):
+            pass
+#            b_values = create a list of stencil values using b
+#            c[i,k] = np.dot(coeffs, b_values)
+    return c
+
+
+dx = 1e3
+dz = 0.5e3
+delta = (dx, dz)
+
+nx = 300
+nz = 50
+n = (nx, nz)
+
+b = np.zeros((nx,nz+1))
+init_schaer_radial(b, b_position)
+
+directory = "build"
+
+c = reconstruct(b)
+
+c_analytic = np.zeros((nx,nz))
+init_schaer_radial(c_analytic, c_position)
+c_error = c - c_analytic
+
+with open(os.path.join(directory, "cerr.dat"), "w") as f:
+    dump(c_error, c_position, f)
